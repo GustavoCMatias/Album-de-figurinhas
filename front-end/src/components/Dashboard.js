68b24c7aec297c328828a7812668c00a3d32f5a3
GustavoCMatias <gustavocmatias@gmail.com>
2023-06-18 13:26:45 -0300

    refactor: dashboard z-index

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { FaUserFriends } from 'react-icons/fa';
 export function Dashboard() {
     const router = useRouter()
     return (
-        <div className="h-[12%] sm:h-1/6 max-h-24 w-full bg-white flex justify-between px-10 fixed top-0 left-0 items-center z-20 border-b-2 border-green-500 "> {/* change h-1/6 ->  h-[12%]*/}
+        <div className="h-[12%] sm:h-1/6 max-h-24 w-full bg-white flex justify-between px-10 fixed top-0 left-0 items-center z-50 border-b-2 border-green-500 "> {/* change h-1/6 ->  h-[12%]*/}
             <p className='text-2xl sm:text-4xl text-green-500 font-bold hover:cursor-pointer' onClick={() => {router.push('/home')}}>Meu Album</p>
             <div className='flex h-full items-center'>
                 <div className='hidden sm:flex'>
